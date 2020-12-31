f6dc555d41323889660c64b059ffd1bb80cf90db
Phạm Công Danh <DanhPC6@fo.com>
2020-12-31 16:37:30 +0700

    edit chart

--- js/pages-jquery.crm_dashboard.init.js
+++ js/pages-jquery.crm_dashboard.init.js
@@ -121,6 +121,256 @@ var plot = $.plot("#CrmDashChart", [{
         },
         colors: ["#2e79f4"]
     };
+
+var plot1 = $.plot("#CrmDashChart1", [{
+        data: flotDataType1,
+        color: "#d9e6fd",
+        lines: {
+            fillColor: {
+                colors: [{
+                    opacity: 1
+                }, {
+                    opacity: .6
+                }]
+            }
+        }
+    }, {
+        data: flotDataType2,
+        color: "#2a76f4",
+        lines: {
+            fillColor: {
+                colors: [{
+                    opacity: 1
+                }, {
+                    opacity: .6
+                }]
+            }
+        }
+    }], {
+        series: {
+            lines: {
+                show: !0,
+                lineWidth: 2,
+                fill: !0,
+                fillColor: {
+                    colors: [{
+                        opacity: 0
+                    }, {
+                        opacity: .3
+                    }]
+                }
+            },
+            gradient: {
+                radial: !0,
+                hoverable: !0,
+                colors: [{
+                    opacity: .5
+                }, {
+                    opacity: 1
+                }]
+            },
+            shadowSize: 4
+        },
+        grid: {
+            hoverable: !1,
+            clickable: !1,
+            borderWidth: 0,
+            labelMargin: 0,
+            color: "rgba(118, 126, 154, 0.4)"
+        },
+        yaxis: {
+            show: !0,
+            min: 0,
+            max: 15,
+            ticks: [
+                [0, ""],
+                [10, "100"],
+                [20, "200"],
+                [30, "300"],
+                [40, "400"],
+                [50, "500"],
+                [60, "600"],
+                [70, "700"]
+            ]
+        },
+        xaxis: {
+            show: !0,
+            ticks: [
+                [0, ""],
+                [10, "Jan"],
+                [22, "Feb"],
+                [34, "Mar"],
+                [46, "Apr"],
+                [58, "May"],
+                [70, "Jun"],
+                [82, "Jul"],
+                [94, "Aug"],
+                [106, "Sep"],
+                [118, "Oct"],
+                [130, "Nov"],
+                [142, "Dec"]
+            ]
+        }
+    }),
+    dash_spark_1 = {
+        chart: {
+            type: "area",
+            height: 25,
+            sparkline: {
+                enabled: !0
+            }
+        },
+        stroke: {
+            curve: "straight",
+            width: 1
+        },
+        fill: {
+            opacity: 1,
+            gradient: {
+                shade: "#fd3c97",
+                type: "vertical",
+                shadeIntensity: .8,
+                inverseColors: !0,
+                opacityFrom: .5,
+                opacityTo: .1,
+                stops: [0, 80, 90],
+                colorStops: []
+            }
+        },
+        series: [{
+            data: [0, 0, 0, 2, 0, 0, 0, 0, 0, 10, 0, 0, 0, 5, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 10, 0, 0, 0, 5, 0, 0, 0, 0, 5, 0, 0, 0, 0]
+        }],
+        yaxis: {
+            min: 0
+        },
+        colors: ["#2e79f4"]
+    };
+
+var plot2 = $.plot("#CrmDashChart2", [{
+        data: flotDataType1,
+        color: "#d9e6fd",
+        lines: {
+            fillColor: {
+                colors: [{
+                    opacity: 1
+                }, {
+                    opacity: .6
+                }]
+            }
+        }
+    }, {
+        data: flotDataType2,
+        color: "#2a76f4",
+        lines: {
+            fillColor: {
+                colors: [{
+                    opacity: 1
+                }, {
+                    opacity: .6
+                }]
+            }
+        }
+    }], {
+        series: {
+            lines: {
+                show: !0,
+                lineWidth: 2,
+                fill: !0,
+                fillColor: {
+                    colors: [{
+                        opacity: 0
+                    }, {
+                        opacity: .3
+                    }]
+                }
+            },
+            gradient: {
+                radial: !0,
+                hoverable: !0,
+                colors: [{
+                    opacity: .5
+                }, {
+                    opacity: 1
+                }]
+            },
+            shadowSize: 4
+        },
+        grid: {
+            hoverable: !1,
+            clickable: !1,
+            borderWidth: 0,
+            labelMargin: 0,
+            color: "rgba(118, 126, 154, 0.4)"
+        },
+        yaxis: {
+            show: !0,
+            min: 0,
+            max: 15,
+            ticks: [
+                [0, ""],
+                [10, "100"],
+                [20, "200"],
+                [30, "300"],
+                [40, "400"],
+                [50, "500"],
+                [60, "600"],
+                [70, "700"]
+            ]
+        },
+        xaxis: {
+            show: !0,
+            ticks: [
+                [0, ""],
+                [10, "Jan"],
+                [22, "Feb"],
+                [34, "Mar"],
+                [46, "Apr"],
+                [58, "May"],
+                [70, "Jun"],
+                [82, "Jul"],
+                [94, "Aug"],
+                [106, "Sep"],
+                [118, "Oct"],
+                [130, "Nov"],
+                [142, "Dec"]
+            ]
+        }
+    }),
+    dash_spark_1 = {
+        chart: {
+            type: "area",
+            height: 25,
+            sparkline: {
+                enabled: !0
+            }
+        },
+        stroke: {
+            curve: "straight",
+            width: 1
+        },
+        fill: {
+            opacity: 1,
+            gradient: {
+                shade: "#fd3c97",
+                type: "vertical",
+                shadeIntensity: .8,
+                inverseColors: !0,
+                opacityFrom: .5,
+                opacityTo: .1,
+                stops: [0, 80, 90],
+                colorStops: []
+            }
+        },
+        series: [{
+            data: [0, 0, 0, 2, 0, 0, 0, 0, 0, 10, 0, 0, 0, 5, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 10, 0, 0, 0, 5, 0, 0, 0, 0, 5, 0, 0, 0, 0]
+        }],
+        yaxis: {
+            min: 0
+        },
+        colors: ["#2e79f4"]
+    };
+
+
 new ApexCharts(document.querySelector("#dash_spark_1"), dash_spark_1).render();
 var dash_spark_2 = {
     chart: {
